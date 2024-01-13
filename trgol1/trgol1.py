f3c132b7e8bd52a539c544be929c1275d280102a
Asmaalafe55 <asmaalafe55@gmail.com>
2024-01-13 23:17:34 +0200

    trgol 1

--- trgol1/trgol1.py
+++ trgol1/trgol1.py
@@ -1,2 +1,16 @@
-print(3)
-print (5)
+
+# יש לפתור מפרק מספר 2 את התרגילים הבאים:  5, 7, 8, 12, 15, 16, 17
+# עבור התרגילים 7 ו- 8 ניתן לכתוב את תוצאת ההדפסה כהערה בקוד
+# יש להגיש קובץ אחד המכיל את כלל הפתורנות ולא קובץ נפרד עבור כל פונקציה.
+
+# מומלץ לפתור את כלל התרגילים בקובץ ולא רק את אלו להגשה לצורך התרגול.
+
+
+# Write a function that will ask the user what his name is
+# and print a message. A name will appear in the message that will be printed
+# the user and at the end also your name:
+
+
+def f1():
+    name = input("Hi, What is your name?")
+    print ("Hello" + name + ", my name is Raz.")
