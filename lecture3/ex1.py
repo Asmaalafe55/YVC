d60ed5743a344ab35ae9ca9f91b3e2dd5465dee3
Asmaalafe55 <asmaalafe55@gmail.com>
2024-01-14 19:09:13 +0200

    lec3

---
+++ lecture3/ex1.py
@@ -0,0 +1,18 @@
+def div(a):
+    if a%2==0:
+        print(a, "is even")
+    else:
+        print(a, "is odd")
+    
+    if a%3==0:
+        print(a, "is divisible by 3")
+    else:
+        print(a, "is not divisible by 3")
+    
+    if a%5==0:
+        print(a, "is divisible by 5")
+    else:
+        print(a, "is not divisible by 5")
+    
+
+div(40)
