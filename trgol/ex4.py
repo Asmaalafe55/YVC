705f14bfabd663d1897ec900df37db139a42b965
Asmaalafe55 <asmaalafe55@gmail.com>
2024-01-19 14:47:28 +0200

    lec4

---
+++ trgol/ex4.py
@@ -0,0 +1,6 @@
+# Write a function that takes an integer as a parameter.
+# It will print for him an appropriate message whether the number is even or not
+# divisible by 3 or not and whether it is divisible by 5 or not
+
+def div235 ():
+    
