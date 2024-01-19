84d6507e3c38acf92d49e165c64511450b80b31b
Asmaalafe55 <asmaalafe55@gmail.com>
2024-01-19 17:54:38 +0200

    the function eq

--- trgol/ex4.py
+++ trgol/ex4.py
@@ -16,8 +16,8 @@ def div235 (num):
     else:
        print (num, "is not divisible by 5")
         
-div235(40)
-div235(39)
+# div235(40)
+# div235(39)
 
 # Write a function that will take two numbers and print a message about 
 # how many of them are positive (greater than zero)  
@@ -29,7 +29,7 @@ def pos(num1, num2):
         print("One of them is positive")
     print ("None of them is positive")
 
-pos(12,0)
+# pos(12,0)
 
 # Receive 3 integers from the user and print the middle one in terms of size.
 
@@ -44,4 +44,27 @@ def med():
     elif num1<=num3<=num2 or num2<=num3<=num1:
         print(num3,"is in the middle")
 
-med()
+# med()
+
+# Write a program that solves a quadratic equation. 
+# The quadratic equation function will take a,b,c and print if there is no solution 
+# or if there are 2 solutions / a single solution and the solutions themselves.
+
+def eq(a,b,c):
+    root = b*b -4*a*c
+    if root<0:
+        print("There is no solution")
+    elif root==0:
+        print("There is one solution : x=",-b/2*a)
+    else:
+        root = root**0.5
+        sol1= (-b - root)/2*a
+        sol2= (-b + root)/2*a
+        print("There is two solutions : ")
+        print("x1 = ", sol1)
+        print("x2 = ", sol2)
+        
+
+eq(2,3,5)
+eq(1,8,16)
+eq(2,-3,1)
