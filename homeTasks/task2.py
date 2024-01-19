06152a032b0677c239b498b4158990421569a26a
Asmaalafe55 <asmaalafe55@gmail.com>
2024-01-19 21:26:55 +0200

    pages function, med function, eq function

--- homeTasks/task2.py
+++ homeTasks/task2.py
@@ -57,3 +57,51 @@ def where():
 # It can be assumed that all the pages are the same, and each line has the same number
 # of words At the end, a report of how many pages will be required will be printed for printing.
 
+def pages():
+    words_per_line = int(input("Words per line? "))
+    lines = int(input("Lines per page? "))
+    words_per_page = int(input("How many words to print? "))
+    pages_num = words_per_page/(words_per_line*lines)
+    if pages_num % 1 != 0 :
+        pages_num = int(pages_num) +1
+    print("We need", int(pages_num), "pages")
+    
+# pages()
+
+# 10) Receive 3 integers from the user and print the middle one in terms of size.
+
+def med():
+    num1 = int(input("enter #1 number: "))
+    num2 = int(input("enter #2 number: "))
+    num3 = int(input("enter #3 number: "))
+    if num1<=num2<=num3 or num3<=num2<=num1:
+        print(num2,"is in the middle")
+    elif num2<=num1<=num3 or num3<=num1<=num2:
+        print(num1,"is in the middle")
+    elif num1<=num3<=num2 or num2<=num3<=num1:
+        print(num3,"is in the middle")
+
+# med()
+
+# 11) Write a program that solves a quadratic equation. 
+# The quadratic equation function will take a,b,c and print if there is no solution 
+# or if there are 2 solutions / a single solution and the solutions themselves.
+
+def eq(a,b,c):
+    root = b*b -4*a*c
+    if root<0:
+        print("There is no solution")
+    elif root==0:
+        print("There is one solution : x=",-b/2*a)
+    else:
+        root = root**0.5
+        sol1= (-b - root)/2*a
+        sol2= (-b + root)/2*a
+        print("There is two solutions : ")
+        print("x1 = ", sol1)
+        print("x2 = ", sol2)
+        
+
+# eq(2,3,5)
+# eq(1,8,16)
+# eq(2,-3,1)
