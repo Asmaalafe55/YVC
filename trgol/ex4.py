2c5c3f8b771bef2ece8c331a5714c2041cfe29c9
Asmaalafe55 <asmaalafe55@gmail.com>
2024-01-19 17:07:09 +0200

    div235

--- trgol/ex4.py
+++ trgol/ex4.py
@@ -2,5 +2,20 @@
 # It will print for him an appropriate message whether the number is even or not
 # divisible by 3 or not and whether it is divisible by 5 or not
 
-def div235 ():
-    
+def div235 (num):
+    if num%2 == 0:
+       print (num, "is even")
+    else:
+       print (num, "is odd")
+    if num%3==0:
+       print (num, "is divisible by 3")
+    else:
+       print (num, "is not divisible by 3")
+    if num%5==0:
+       print (num, "is divisible by 5")
+    else:
+       print (num, "is not divisible by 5")
+        
+        
+        
+        
