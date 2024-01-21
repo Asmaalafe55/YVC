5da3afaae43bb5d17f5261ecec0010d5189baade
Asmaalafe55 <asmaalafe55@gmail.com>
2024-01-21 14:40:26 +0200

    g function

--- trgol/ex4-1.py
+++ trgol/ex4-1.py
@@ -1,3 +1,11 @@
 # 6) Write a function that will receive a word from the user and an 
 # integer n and print the word n times, in separate lines
 
+def g():
+    x = input("enter a word: ")
+    n = int(input("how many times: "))
+    for i in range(n):
+        print(x)
+
+# g()
+
