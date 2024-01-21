7a29c098627b32fefb55c497adbc1de01b63789d
Asmaalafe55 <asmaalafe55@gmail.com>
2024-01-21 15:24:45 +0200

    ff function

--- trgol/ex4-1.py
+++ trgol/ex4-1.py
@@ -27,3 +27,35 @@ def zero():
         
 # zero()
 
+# 9) Take a string and return the first uppercase letter that appears in it.
+# If there is no uppercase letter return an empty string.
+
+def f(strr):
+    capital = "QWERTYUIOPASDFGHJKLZXCVBNM"
+    for i in strr:
+        if i in capital:
+            return i
+    return ""
+
+# print(f("1+asdf 2-QfejjDRB"))
+
+# 10) Take a string (lower case only) return the first letter of the alphabet that appears in it.
+
+def ff(strr):
+    smaller = strr[0]
+    for l in strr:
+        if l<smaller:
+            smaller=l
+    return smaller
+        
+# print(ff("qwerty"))
+
+# 11) Receive an integer N from the user and print N lines from the following
+# template (note that after that the whole number is not printed)
+
+
+
+# 12) Get the width and height of a rectangle from the user and print a rectangle
+# like in the example, with the rows containing "X" or "O" alternately only.
+# The first line will always be "X", regardless of the numbers entered by the user,
+# which only determine the size of the rectangle.
