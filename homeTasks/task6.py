476b25b443b3af35723936ff2b802abcb97e785c
Asmaalafe55 <asmaalafe55@gmail.com>
2024-02-24 15:41:45 +0200

    f2 function , f5 funcrion

--- homeTasks/task6.py
+++ homeTasks/task6.py
@@ -1,4 +1,41 @@
+# 2) 
+def hezqa(n):
+    return 2**n
+def sequence(j,s,i):
+    j = str(j)
+    if j*2 in s:
+        return i
+    return False
+def f2():
+    num = []*101
+    for i in range(101):
+        num += [str(hezqa(i))]
+    for j in range(10):
+        k=str(j)
+        print ('The sequence in',k*2,'appers in these powers of 2: ', end ='')
+        for i in range(101):
+            if sequence(j, num[i],i):
+                print (i,' ', end ='')
+        print()
+            
+# f2()
 
+# 5)
+
+def f5(L1,L2):
+    counter=0 
+    for i in range(len(L1)):
+        for j in range(len(L2)):
+            if L1[i]*L2[j]>48 and L1[i]*L2[j]<76:
+                counter+=1
+    return counter
+
+# print(f5([1,2,5,7,10],[2,7,8,11]))
+
+# 6) 
+
+
+# 23) 
 def f23(L):
     print(L)
     cols = len(range(L[0]))
