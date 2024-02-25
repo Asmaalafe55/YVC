1a3dfec666c4a44133f85f5a20dcde4604348064
Asmaalafe55 <asmaalafe55@gmail.com>
2024-02-25 10:37:14 +0200

    lec 9

---
+++ lectures/lec9-1.py
@@ -0,0 +1,19 @@
+# to convert all the small letters to capital
+# x= 'dsbjkhkaSACDS dj34 3dD'
+# print(x.upper())
+
+# to convert all the capital letters to small letters
+# x= 'dsbjkhkaSACDS dj34 3dD'
+# print (x.lower())
+
+# to make only the first letter is capital
+# x= 'dsbjkhkaSACDS dj34 3dD'
+# print(x.capitalize())
+
+# to make every word start with capital letter
+# x= 'dsbjkhkaSACDS dj34 3dD'
+# print(x.title())
+
+# to swap the small letters to be capital and the capital to be small letters
+# x= 'dsbjkhkaSACDS dj34 3dD'
+# print(x.swapcase())
