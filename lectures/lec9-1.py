5efa3cfff147d583c4d7dd593f33a18766576ebd
Asmaalafe55 <asmaalafe55@gmail.com>
2024-02-25 10:37:29 +0200

    f1 function

--- lectures/lec9-1.py
+++ lectures/lec9-1.py
@@ -1,19 +1,28 @@
 # to convert all the small letters to capital
-# x= 'dsbjkhkaSACDS dj34 3dD'
+# x= 'heLLO hi 12!'
 # print(x.upper())
+# print(x.isupper()) -- True/False
+
 
 # to convert all the capital letters to small letters
-# x= 'dsbjkhkaSACDS dj34 3dD'
+# x= 'heLLO hi 12!'
 # print (x.lower())
+# print (x.islower()) -- True/False
+
 
 # to make only the first letter is capital
-# x= 'dsbjkhkaSACDS dj34 3dD'
+# x= 'heLLO hi 12!'
 # print(x.capitalize())
 
+
 # to make every word start with capital letter
-# x= 'dsbjkhkaSACDS dj34 3dD'
+# x= 'heLLO hi 12!'
 # print(x.title())
 
 # to swap the small letters to be capital and the capital to be small letters
-# x= 'dsbjkhkaSACDS dj34 3dD'
+# x= 'heLLO 12!'
 # print(x.swapcase())
+
+def f1():
+    for i in range(1,13,2):
+        print((i*'X').center(11))
