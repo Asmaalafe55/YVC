bc51460dfbef39c89a379061d0b6c174aaca1f15
Asmaalafe55 <asmaalafe55@gmail.com>
2024-04-30 13:18:13 +0300

    lec 2 first ex

---
+++ java_assignments/lectures/lec2/Applications.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+class Applications {
+    public static void main(String[] args) {
+        
+        int[] numbers = new int[5];
+        int positiveNumbers = 0;
+        int counter =0;
+        
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter a text");
+            for (int i=0 ; i<5; i++){
+                numbers[i] = scanner.nextInt();
+                if (numbers[i] >0 ){
+                    positiveNumbers += numbers[i];
+                    counter ++;
+                }
+            }
+        }
+        System.out.println("the sum of the positive numbers that you entered : "+ positiveNumbers + "\nthe number of the positive numbers you entered is : "+ counter);
+    
+    }
+}
