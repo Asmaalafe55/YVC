8d117e9e7c144192820c5ff4798cd8f1c1e3eff1
Asmaalafe55 <asmaalafe55@gmail.com>
2024-04-30 14:41:23 +0300

    prettier

--- java_assignments/trgol/ex2/Application.java
+++ java_assignments/trgol/ex2/Application.java
@@ -3,42 +3,36 @@ package trgol.ex2;
 import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        
-        // to scan unntil 5 strings and to put it in an array
 
-        // to check if the array have equals abc 
-        String[] arr = new String[5];
-        int sumi =0;
-        System.out.println("Input 5 strings (seperated with enter): ");
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
 
-        for (int i =0 ; i< 5; i++ ){
-        System.out.println("Input a string: ");
+    // to scan unntil 5 strings and to put it in an array
 
-            arr[i] = input.nextLine();
-        System.out.println("Input a number: ");
-        sumi += input.nextInt();
+    // to check if the array have equals abc
+    String[] arr = new String[5];
+    int sumi = 0;
+    System.out.println("Input 5 strings (seperated with enter): ");
 
- 
-        }
-        System.out.println(" ");
+    for (int i = 0; i < 5; i++) {
+      System.out.println("Input a string: ");
 
-        for (int i =0; i<5 ; i++){
-            if (arr[i].equals("abc")){
-                System.out.println("Pingo");
-                break;
-            }
-        }
-        System.out.println("oh oh");
-        System.out.println("The root of sum " + Math.sqrt(sumi));
-        if (Math.sqrt(sumi)>=5){
-        System.out.println("Yofe");
-        }
-
-
-
-    
+      arr[i] = input.nextLine();
+      System.out.println("Input a number: ");
+      sumi += input.nextInt();
     }
-    
+    System.out.println(" ");
+
+    for (int i = 0; i < 5; i++) {
+      if (arr[i].equals("abc")) {
+        System.out.println("Pingo");
+        break;
+      }
+    }
+    System.out.println("oh oh");
+    System.out.println("The root of sum " + Math.sqrt(sumi));
+    if (Math.sqrt(sumi) >= 5) {
+      System.out.println("Yofe");
+    }
+  }
 }
