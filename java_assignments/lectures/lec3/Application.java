a60bc8c55541246d5080279c34e8edb8df3d0627
Asmaalafe55 <asmaalafe55@gmail.com>
2024-05-07 09:33:07 +0300

    bank account class

--- java_assignments/lectures/lec3/Application.java
+++ java_assignments/lectures/lec3/Application.java
@@ -2,6 +2,12 @@ package lectures.lec3;
 
 public class Application {
     public static void main(String[] args) {
-        
+        BankAccount account = new BankAccount();
+        System.out.println(account.value);
+        account.value = 2000;
+        System.out.println(account.value);
+
+        account.printAccountReport();
+
     }
 }
---
+++ java_assignments/lectures/lec3/BankAccount.java
@@ -0,0 +1,10 @@
+package lectures.lec3;
+
+public class BankAccount {
+    public int value=100;
+
+    public void printAccountReport(){
+        System.out.println("Number account number is : " + value);
+        
+    }
+}
