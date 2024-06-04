2f590ce971dadef17188196879bb40f82d7dc878
Asmaalafe55 <asmaalafe55@gmail.com>
2024-06-04 15:28:40 +0300

    q2

---
+++ java_assignments/trgol/ex6/Application_Q2.java
@@ -0,0 +1,32 @@
+package trgol.ex6;
+
+public class Application_Q2 {
+
+  public static void main(String[] args) {
+    System.out.println(power(2, 4));
+    System.out.println(isPower(25, 5));
+    System.out.println(digitSum(34));
+  }
+
+  public static int power(int base, int exponent) {
+    int powerr = 1;
+    for (int i = 0; i < exponent; i++) {
+      powerr *= base;
+    }
+    return powerr;
+  }
+
+  public static boolean isPower(int result, int base) {
+    if (base * base == result) return true;
+    return false;
+  }
+
+  public static int digitSum(int num) {
+    int sumi = 0;
+    while (num != 0) {
+      sumi += num % 10;
+      num /= 10;
+    }
+    return sumi;
+  }
+}
