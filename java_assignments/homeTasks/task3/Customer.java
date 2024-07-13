2555ccfd45bf108ff554037c19afed0b690acaea
Asmaalafe55 <asmaalafe55@gmail.com>
2024-07-13 19:15:36 +0300

    adding comments to customer class

--- java_assignments/homeTasks/task3/Customer.java
+++ java_assignments/homeTasks/task3/Customer.java
@@ -1,6 +1,10 @@
 package homeTasks.task3;
 
+// Represents a Customer, extending AbstractPerson.
+
 public class Customer extends AbstractPerson {
+
+  // Constructor for initializing a Customer with name, id, address, phone, and email.
 
   public Customer(
     String name,
@@ -9,12 +13,20 @@ public class Customer extends AbstractPerson {
     String phone,
     String email
   ) {
-    super(name, id, address, phone, email);
+    super(name, id, address, phone, email); // Call to superclass constructor
   }
 
+  // Constructor for initializing a Customer with memberId.
+  // Uses default values for name, address, phone, and email.
+
   public Customer(int memberId) {
-    super("Customer name", memberId, "Customer address", "0", "Customer email");
+    super("Customer name", memberId, "Customer address", "0", "Customer email"); // Call to superclass constructor
   }
+
+  // Method to add a phone number for the Customer.
+  // Validates that the phone number has exactly 10 digits.
+  // phone The phone number to add.
+  // return true if the phone number is valid and added, false otherwise.
 
   public boolean addPhoneNumber(String phone) {
     if (phone.length() != 10) {
@@ -23,10 +35,11 @@ public class Customer extends AbstractPerson {
       );
       return false;
     }
-    this.phone = phone;
+    this.phone = phone; // Set the phone number
     return true;
   }
 
+  // Getters and setters for Customer attributes
   public String getName() {
     return name;
   }
@@ -66,6 +79,9 @@ public class Customer extends AbstractPerson {
   public void setEmail(String email) {
     this.email = email;
   }
+
+  // Override of toString method to provide a string representation of Customer.
+  // return String representation of Customer object.
 
   @Override
   public String toString() {
