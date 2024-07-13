e864b194cb9e5d79775468965055baba8142d030
Asmaalafe55 <asmaalafe55@gmail.com>
2024-07-13 19:22:19 +0300

    comments on movie item class

--- java_assignments/homeTasks/task3/MovieItem.java
+++ java_assignments/homeTasks/task3/MovieItem.java
@@ -2,10 +2,12 @@ package homeTasks.task3;
 
 public class MovieItem extends LibraryItem {
 
+  // Constructor to initialize a movie item with artist, year published, and genre
   public MovieItem(String artist, int yearPublished, String genre) {
-    super(artist, yearPublished, genre);
+    super(artist, yearPublished, genre); // Call to superclass constructor to initialize attributes
   }
 
+  // Override of toString method to provide a string representation of the movie item
   @Override
   public String toString() {
     return (
