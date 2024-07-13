a3a2c2ff18798e0cf9e755aa06e28dc6e2dd2a62
Asmaalafe55 <asmaalafe55@gmail.com>
2024-07-13 19:13:51 +0300

    adding comments on cd item class

--- java_assignments/homeTasks/task3/CdItem.java
+++ java_assignments/homeTasks/task3/CdItem.java
@@ -1,10 +1,16 @@
 package homeTasks.task3;
 
+// Represents a CD item extending LibraryItem.
+
 public class CdItem extends LibraryItem {
 
+  // Constructor for initializing a CD item with artist, year published, and genre.
+
   public CdItem(String artist, int yearPublished, String genre) {
-    super(artist, yearPublished, genre);
+    super(artist, yearPublished, genre); // Call to superclass constructor
   }
+
+  // Override of toString method to provide a string representation of CdItem.
 
   @Override
   public String toString() {
