e91da34c2a672adee9103a9a9c4d3e5077b87d61
Asmaalafe55 <asmaalafe55@gmail.com>
2024-07-13 19:21:40 +0300

    comments on library item class

--- java_assignments/homeTasks/task3/LibraryItem.java
+++ java_assignments/homeTasks/task3/LibraryItem.java
@@ -2,10 +2,12 @@ package homeTasks.task3;
 
 public class LibraryItem {
 
-  protected String artist;
-  protected int yearPublished;
-  protected String genre;
+  // Attributes to store details of the library item
+  protected String artist; // Artist or creator of the item
+  protected int yearPublished; // Year when the item was published or released
+  protected String genre; // Genre or category of the item
 
+  // Constructor to initialize the library item with artist, year published, and genre
   public LibraryItem(String artist, int yearPublished, String genre) {
     this.artist = artist;
     this.yearPublished = yearPublished;
