57c789204d1de4211aeae648ececdda886d5c70b
Asmaalafe55 <asmaalafe55@gmail.com>
2024-07-13 19:11:05 +0300

    adding comments on book item class

--- java_assignments/homeTasks/task3/BookItem.java
+++ java_assignments/homeTasks/task3/BookItem.java
@@ -1,10 +1,18 @@
 package homeTasks.task3;
 
+// Represents a specific type of library item, a book.
+// Inherits attributes from LibraryItem and provides a specific toString() method.
+
 public class BookItem extends LibraryItem {
 
+  // Constructor to initialize a BookItem with artist, year published, and genre.
+
   public BookItem(String artist, int yearPublished, String genre) {
+    // Call the constructor of LibraryItem with provided parameters
     super(artist, yearPublished, genre);
   }
+
+  // Overrides the toString() method to provide a formatted string representation of a BookItem.
 
   @Override
   public String toString() {
